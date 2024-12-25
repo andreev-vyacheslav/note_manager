ae957214856487b70505cca3ce1ca73883c642f9
andreev-vyacheslav <142881263+andreev-vyacheslav@users.noreply.github.com>
2024-12-25 20:14:55 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Login"
+title = "Заголовок заметки"
+content = "Описание заметки"
+status = "Статус заметки"
+created_date = "01.01.2000"
+issue_date = "01.03.2000"
+
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("Дата истечения:", issue_date)
